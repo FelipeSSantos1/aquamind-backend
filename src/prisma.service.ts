ab494157894a888b8a5e673325645f70fa64180a
Felipe Santos <felipemillhouse@gmail.com>
2021-09-29 17:42:25 -0400

    eslint prettier

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,18 +3,18 @@ import {
   Injectable,
   OnModuleInit,
   //   OnModuleDestroy,
-} from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+} from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect()
   }
 
   async enableShutdownHooks(app: INestApplication) {
-    this.$on("beforeExit", async () => {
-      await app.close();
-    });
+    this.$on('beforeExit', async () => {
+      await app.close()
+    })
   }
 }

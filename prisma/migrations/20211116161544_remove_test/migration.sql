0a24582ea28f26111fd360d4acc4f582fa345f00
Felipe Santos <felipemillhouse@gmail.com>
2021-11-16 11:15:53 -0500

    remove test

---
+++ prisma/migrations/20211116161544_remove_test/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `testMethod` on the `Fertilizer` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Fertilizer" DROP COLUMN "testMethod";

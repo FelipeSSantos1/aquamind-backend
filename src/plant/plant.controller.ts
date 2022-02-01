ba1ce24dbf9fab0788374d6907a2a36b19ac6c60
Felipe Santos <felipemillhouse@gmail.com>
2022-02-01 11:47:40 -0500

    increase get plant by id requests by minute

--- src/plant/plant.controller.ts
+++ src/plant/plant.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common'
+import { Throttle } from '@nestjs/throttler'
 import { JwtAuthGuard } from 'src/auth/jwtAuth.guard'
 import { GetByNameDto, PlantIdDto } from './dto/plant.dto'
 import { PlantService } from './plant.service'
@@ -14,6 +15,7 @@ export class PlantController {
   }
 
   @Get(':id')
+  @Throttle(30, 60)
   @UseGuards(JwtAuthGuard)
   getById(@Param() { id }: PlantIdDto) {
     return this.plantService.getById({ id })
